a4cd505fbb10f2bcf6361fd278f24880f1ebbf87
Jannatul Kabir <142692288+jannatul02@users.noreply.github.com>
2023-10-27 12:52:12 +0600

    Updated dashboard

--- dashboard.php
+++ dashboard.php
@@ -230,6 +230,12 @@
 
       gtag("config", "UA-23581568-13");
     </script>
-    
+    <script
+      defer
+      src="https://static.cloudflareinsights.com/beacon.min.js/v52afc6f149f6479b8c77fa569edb01181681764108816"
+      integrity="sha512-jGCTpDpBAYDGNYR5ztKt4BQPGef1P0giN6ZGVUi835kFF88FOmmn8jBQWNgrNd8g/Yu421NdgWhwQoaOPFflDw=="
+      data-cf-beacon='{"rayId":"7c117abcafc291ab","version":"2023.4.0","b":1,"token":"cd0b4b3a733644fc843ef0b185f98241","si":100}'
+      crossorigin="anonymous"
+    ></script>
   </body>
 </html>
