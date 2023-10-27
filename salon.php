0703597c5c057b56feb8c468c52327258ef13f1b
Benzamin Yasir <benzamin.siam@northsouth.edu>
2023-10-27 14:42:30 +0600

    Reservation for Salon

--- salon.php
+++ salon.php
@@ -30,69 +30,69 @@ include 'frontend_query.php';
     <link rel="stylesheet" href="./Starbase/common.css" />
 
     <script nonce="b3451030-54b2-447a-9bcf-c29babb905d2">
-    (function(w, d) {
-        ! function(dk, dl, dm, dn) {
-            dk[dm] = dk[dm] || {};
-            dk[dm].executed = [];
-            dk.zaraz = {
-                deferred: [],
-                listeners: []
-            };
-            dk.zaraz.q = [];
-            dk.zaraz._f = function(dp) {
-                return function() {
-                    var dq = Array.prototype.slice.call(arguments);
-                    dk.zaraz.q.push({
-                        m: dp,
-                        a: dq
-                    })
-                }
-            };
-            for (const dr of ["track", "set", "debug"]) dk.zaraz[dr] = dk.zaraz._f(dr);
-            dk.zaraz.init = () => {
-                var ds = dl.getElementsByTagName(dn)[0],
-                    dt = dl.createElement(dn),
-                    du = dl.getElementsByTagName("title")[0];
-                du && (dk[dm].t = dl.getElementsByTagName("title")[0].text);
-                dk[dm].x = Math.random();
-                dk[dm].w = dk.screen.width;
-                dk[dm].h = dk.screen.height;
-                dk[dm].j = dk.innerHeight;
-                dk[dm].e = dk.innerWidth;
-                dk[dm].l = dk.location.href;
-                dk[dm].r = dl.referrer;
-                dk[dm].k = dk.screen.colorDepth;
-                dk[dm].n = dl.characterSet;
-                dk[dm].o = (new Date).getTimezoneOffset();
-                if (dk.dataLayer)
-                    for (const dy of Object.entries(Object.entries(dataLayer).reduce(((dz, dA) => ({
+        (function (w, d) {
+            ! function (dk, dl, dm, dn) {
+                dk[dm] = dk[dm] || {};
+                dk[dm].executed = [];
+                dk.zaraz = {
+                    deferred: [],
+                    listeners: []
+                };
+                dk.zaraz.q = [];
+                dk.zaraz._f = function (dp) {
+                    return function () {
+                        var dq = Array.prototype.slice.call(arguments);
+                        dk.zaraz.q.push({
+                            m: dp,
+                            a: dq
+                        })
+                    }
+                };
+                for (const dr of ["track", "set", "debug"]) dk.zaraz[dr] = dk.zaraz._f(dr);
+                dk.zaraz.init = () => {
+                    var ds = dl.getElementsByTagName(dn)[0],
+                        dt = dl.createElement(dn),
+                        du = dl.getElementsByTagName("title")[0];
+                    du && (dk[dm].t = dl.getElementsByTagName("title")[0].text);
+                    dk[dm].x = Math.random();
+                    dk[dm].w = dk.screen.width;
+                    dk[dm].h = dk.screen.height;
+                    dk[dm].j = dk.innerHeight;
+                    dk[dm].e = dk.innerWidth;
+                    dk[dm].l = dk.location.href;
+                    dk[dm].r = dl.referrer;
+                    dk[dm].k = dk.screen.colorDepth;
+                    dk[dm].n = dl.characterSet;
+                    dk[dm].o = (new Date).getTimezoneOffset();
+                    if (dk.dataLayer)
+                        for (const dy of Object.entries(Object.entries(dataLayer).reduce(((dz, dA) => ({
                             ...dz[1],
                             ...dA[1]
                         }))))) zaraz.set(dy[0], dy[1], {
-                        scope: "page"
-                    });
-                dk[dm].q = [];
-                for (; dk.zaraz.q.length;) {
-                    const dB = dk.zaraz.q.shift();
-                    dk[dm].q.push(dB)
-                }
-                dt.defer = !0;
-                for (const dC of [localStorage, sessionStorage]) Object.keys(dC || {}).filter((dE => dE
-                    .startsWith("_zaraz_"))).forEach((dD => {
-                    try {
-                        dk[dm]["z_" + dD.slice(7)] = JSON.parse(dC.getItem(dD))
-                    } catch {
-                        dk[dm]["z_" + dD.slice(7)] = dC.getItem(dD)
+                            scope: "page"
+                        });
+                    dk[dm].q = [];
+                    for (; dk.zaraz.q.length;) {
+                        const dB = dk.zaraz.q.shift();
+                        dk[dm].q.push(dB)
                     }
-                }));
-                dt.referrerPolicy = "origin";
-                dt.src = "../../cdn-cgi/zaraz/sd0d9.js?z=" + btoa(encodeURIComponent(JSON.stringify(dk[dm])));
-                ds.parentNode.insertBefore(dt, ds)
-            };
-            ["complete", "interactive"].includes(dl.readyState) ? zaraz.init() : dk.addEventListener(
-                "DOMContentLoaded", zaraz.init)
-        }(w, d, "zarazData", "script");
-    })(window, document);
+                    dt.defer = !0;
+                    for (const dC of [localStorage, sessionStorage]) Object.keys(dC || {}).filter((dE => dE
+                        .startsWith("_zaraz_"))).forEach((dD => {
+                            try {
+                                dk[dm]["z_" + dD.slice(7)] = JSON.parse(dC.getItem(dD))
+                            } catch {
+                                dk[dm]["z_" + dD.slice(7)] = dC.getItem(dD)
+                            }
+                        }));
+                    dt.referrerPolicy = "origin";
+                    dt.src = "../../cdn-cgi/zaraz/sd0d9.js?z=" + btoa(encodeURIComponent(JSON.stringify(dk[dm])));
+                    ds.parentNode.insertBefore(dt, ds)
+                };
+                ["complete", "interactive"].includes(dl.readyState) ? zaraz.init() : dk.addEventListener(
+                    "DOMContentLoaded", zaraz.init)
+            }(w, d, "zarazData", "script");
+        })(window, document);
     </script>
 </head>
 
@@ -121,17 +121,17 @@ include 'frontend_query.php';
                 <li><a href="./parlor.php">PARLOR</a></li>
                 <li>
                     <?php if ($_SESSION['member_logged_in'] == true) { ?>
-                    <div class="dropdown">
-                        <a style="color: #fff;" class="active">
+                        <div class="dropdown">
+                            <a style="color: #fff;" class="active">
                             <?php echo strtoupper($_SESSION['memberName']); ?>
                         </a>
-                        <div class="dropdown-content">
-                            <a class="dropdown-item" href="<?= $baseURL ?>/dashboard.php">Dashboard</a>
+                            <div class="dropdown-content">
+                                <a class="dropdown-item" href="<?= $baseURL ?>/dashboard.php">Dashboard</a>
                             <a class="dropdown-item" href="<?= $baseURL ?>/frontend_logout.php">Logout</a>
+                            </div>
                         </div>
-                    </div>
                     <?php } else { ?>
-                    <a href="./login.php">LOGIN</a>
+                        <a href="./login.php">LOGIN</a>
                     <?php } ?>
                 </li>
                 <!-- <li><a href="./admin/index.php">ADMIN</a></li> -->
@@ -258,6 +258,36 @@ include 'frontend_query.php';
         </div>
     </div>
 
+    <section class="services-section spad">
+        <div class="container2">
+            <div class="about-us">
+                <h2>BOOK AN APPOINTMENT AT</h2>
+                <h1>Starbase Salon</h1>
+            </div>
+            <div class="main">
+                <input type="checkbox" id="chk" aria-hidden="true">
+
+                <div class="signup">
+                    <form action="frontend_query.php" method="post">
+                        <label for="chk" aria-hidden="true">Booking</label>
+                        <input type="text" name="name" placeholder="Name" required="">
+                        <input type="email" name="email" placeholder="Email" required="">
+                        <input type="text" name="phone" placeholder="Phone" required="">
+                        <input type="date" name="bdate" required />
+                        <input type="time" name="btime" required />
+                        <select type="text" name="service" required>
+                            <option value="" disabled selected>Service Type</option>
+                            <?php while ($row = mysqli_fetch_assoc($service_query)) { ?>
+                                <option value="<?= $row['sid'] ?>"><?= $row['s_name']; ?></option>
+                            <?php } ?>
+                        </select>
+                        <textarea placeholder="Additional Notes" name="note"></textarea>
+                        <button type="submit" name="save">Reserve</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <footer class="footer-section">
         <div class="upper">
@@ -305,14 +335,14 @@ include 'frontend_query.php';
 
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
+        window.dataLayer = window.dataLayer || [];
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-    gtag('config', 'UA-23581568-13');
+        gtag('config', 'UA-23581568-13');
     </script>
     <script defer
         src="https://static.cloudflareinsights.com/beacon.min.js/v52afc6f149f6479b8c77fa569edb01181681764108816"
