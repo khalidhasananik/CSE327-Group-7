a7c9784eee44dbc96aee39a2b6c59dfeeae5a2be
Sumaiya-1234 <139035857+Sumaiya-1234@users.noreply.github.com>
2023-10-29 01:15:20 +0600

    Member: index updated

--- admin/members/index.php
+++ admin/members/index.php
@@ -1,9 +1,11 @@
 <?php
     // ini_set('display_errors', 1);
-    include '../../config/db_connection.php';
-    include '../../config/constants.php';
+    // Include necessary files and set up configurations
+    include '../../config/db_connection.php';// Include the database connection file
+    include '../../config/constants.php';// Include constants and configuration settings
 
-    if ($_SESSION['admin_logged_in'] == true) {
+    // Check if the admin is logged in
+    if ($_SESSION['admin_logged_in'] == true) { // Include additional queries specific to this page
         include 'query.php';
 ?>
 <!doctype html>
@@ -28,7 +30,7 @@
   </head>
   <body>
     <?php
-        include '../nav.php';
+        include '../nav.php';// Include the navigation bar specific to this page
     ?>
 
     <div class="container">
@@ -60,7 +62,8 @@
                 </tr>
               </thead>
               <tbody>
-                <?php $id = 1; while ($row = mysqli_fetch_assoc($s_query)) {?>            
+                <?php $id = 1; while ($row = mysqli_fetch_assoc($s_query)) {?>  
+                  <!-- Display member details in table rows -->          
                 <tr class="middle-td">
                   <td scope="row"><?= $id++;?></td>
                   <td><?= $row['mname']; ?></td>
@@ -70,6 +73,7 @@
                   <td><?= $row['pname'] . ' (' . intval($row['month_duration']) . ' month @ TAKA ' . $row['price'] . ')' ?></td>
                   <td><?= $row['discount']; ?></td>
                   <td>
+                    <!-- Edit and Delete buttons for each member -->
                     <div class="d-flex">
                       <a href=<?= "updateMember.php?member_id=$row[mid]" ?>  class="btn btn-success m-1">Edit</a>
                       <form action="<?= "query.php?member_id=$row[mid]" ?>" method="post" onsubmit="return confirm('Do you really want to delete the member?');">
@@ -96,6 +100,7 @@
 
 <?php
     } else {
+       // If the admin is not logged in, redirect to the login page
         header("location:$baseURL/admin/index.php?msg=login_first");
     }
 ?>
