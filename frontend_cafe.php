2fe5bd48cb560551965cda1835469311e64def6c
Benzamin Yasir <benzamin.siam@northsouth.edu>
2023-10-29 17:40:15 +0600

    Updated Comments

--- frontend_cafe.php
+++ frontend_cafe.php
@@ -1,25 +1,27 @@
 <?php
 
-    // ini_set('display_errors', 1);
-    include './config/db_connection.php';
-    include './config/constants.php';
+// ini_set('display_errors', 1);
+include './config/db_connection.php';
+include './config/constants.php';
 
-    if (isset($_POST['save'])) {	
-	    $name= $_POST['name'];
-	    $email= $_POST['email'];
-	    $phone= $_POST['phone'];
-	    $service= $_POST['service'];
-        $note= $_POST['note'];
-        $date= $_POST['bdate'];
-        $time= $_POST['btime'];
+//The data from the web page is sent to the database
+if (isset($_POST['save'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $service = $_POST['service'];
+    $note = $_POST['note'];
+    $date = $_POST['bdate'];
+    $time = $_POST['btime'];
 
-        $sql_query = "INSERT INTO reservation(Name, Email, Phone, Additional_Notes,Site, Number_of_Seats, Reservation_Date, Reservation_Time)
+    $sql_query = "INSERT INTO reservation(Name, Email, Phone, Additional_Notes,Site, Number_of_Seats, Reservation_Date, Reservation_Time)
             VALUES ('$name','$email','$phone','$note','Cafe','$service','$date','$time')";
 
-        if (mysqli_query($conn, $sql_query)) {
-            echo "<script>alert('Reservation created successfully'); window.location.href='$baseURL/cafe.php';</script>";
-        } else {
-            echo "<script>alert('Something went wrong... Please try again!!'); window.location.href='$baseURL/cafe.php';</script>";
-        }
+    //message to be displayed after the query is executed
+    if (mysqli_query($conn, $sql_query)) {
+        echo "<script>alert('Reservation created successfully'); window.location.href='$baseURL/cafe.php';</script>";
+    } else {
+        echo "<script>alert('Something went wrong... Please try again!!'); window.location.href='$baseURL/cafe.php';</script>";
     }
+}
 ?>
